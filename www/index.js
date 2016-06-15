c3e6b53cf0ed079cfe50de8e107c1012925e6eb9
edvaldig <edvaldig@gmail.com>
2016-06-15 15:28:14 +0000

    ...

--- www/index.js
+++ www/index.js
@@ -2,8 +2,15 @@
 /// <reference path="typings/typings/globals/d3/index.d.ts" />
 var mapdata = null;
 var tree = null;
+var teamdata = null;
 
 var EARTH_RADIUS = 6371.0e3;
+
+var markers = [
+    {name:"Akureyri", latitude:65.700359, longitude:-18.141921 }
+] 
+
+
 
 function latlonToPoint(p) {
     var rad = Math.PI/180;
@@ -16,7 +23,7 @@ function latlonToPoint(p) {
     return {x,y,z};
 }
 
-function distance2(a,b) {
+function distance(a,b) {
     var x1 = a.x,
         x2 = b.x,
         y1 = a.y,
@@ -28,10 +35,26 @@ function distance2(a,b) {
 
 }
 
-function buildMap(error, map) {
-    for(var i = 0; i<map.length; i++) {
-        var d = map[i];
+function interpolateElement(p1, p2, f, t) {
+    return f(p1) * (t) + f(p2) * (1-t);
+}
 
+function interpolateMapPoints(p1, p2, t) {
+    return {
+        p1, 
+        p2, 
+        distkmTotal:interpolateElement(p1, p2, x=>x.distkmTotal, t),
+        distkmTotalFixed:interpolateElement(p1, p2, x=>x.distkmTotalFixed, t),
+        pct:interpolateElement(p1, p2, x=>x.pct, t), 
+        elevation:interpolateElement(p1, p2, x=>x.elevation, t),
+        longitude: interpolateElement(p1, p2, x=>x.longitude, t),
+        latitude: interpolateElement(p1, p2, x=>x.latitude, t)    
+    }
+}
+
+function parseMapData(map) {
+   for(var i = 0; i<map.length; i++) {
+        var d = map[i];
         d.idx = parseInt(d.idx);
         d.distkm = parseFloat(d.distkm);
         d.distkmTotal = parseFloat(d.distkmTotal);
@@ -48,18 +71,76 @@ function buildMap(error, map) {
         d.prev = i > 0 ? map[i-1] : null;
         d.next = i == map.length ? null : map[i+1];
     }
-    
-    mapdata = map;
-    
-    var p = map[300];
 
-    tree = new kdTree(mapdata, distance2, ["x", "y", "z"]);
-    console.log(tree.nearest(latlonToPoint(p), 1)[0]);
-    console.log(p);
+    return map;
+}
+
+function parseTeamData(teams) {
+    return teams.map(t=>{
+        var lon = parseFloat(t.Longitude.replace(',', '.'));
+        var lat = parseFloat(t.Latitude.replace(',', '.'));
+        var cart = latlonToPoint({latitude:lat, longitude:lon});
+
+        return {
+            numberPlate:t.CarLicense,
+            name:t.Description,
+            category:t.Category,
+            categoryName:t.CategoryName,
+            longitude:lon,
+            latitude:lat,
+            cartesian:cart,
+            x:cart.x,
+            y:cart.y,
+            z:cart.z,
+            nosignal:t.NoSignal, 
+            speed:parseFloat(t.Speed.replace(',', '.')) };
+    });
+}
+
+function approximatePosition(p, tree) {
+    var n, dn;
+    var x = 'x' in p ? p : latlonToPoint(p);
+    [n,dn] = tree.nearest(x, 1)[0];
+    var dnn = n.next == null ? 1e9 : distance(x, n.next);
+    var dnp = n.prev == null ? 1e9 : distance(x, n.prev);
+    // console.log(`distance to closest: ${dn} meters`);
+    // console.log(`distance to next: ${dnn} meters`);
+    // console.log(`distance to previous: ${dnp} meters`);
+
+    if(dnn < dnp) { // we're past n
+        var t = dn / (dn + dnn);   
+        return approx = interpolateMapPoints(n.next, n, t);
+    } else { // we havent reached n 
+        var t = dn / (dn + dnp);
+        return approx = interpolateMapPoints(n.prev, n, t);
+    } 
+
+}
+
+function buildMap(error, map, teams) {
+    mapdata = parseMapData(map);
+    teamdata = parseTeamData(teams);
+    var p = teamdata[90];
+
+    tree = new kdTree(mapdata, distance, ["x", "y", "z"]);
+    
+    var approx = approximatePosition(p, tree);
+
+    markers.forEach(x=>{
+        x.routePoint = approximatePosition(x, tree);
+    });
+
+
+    teamdata.forEach(t=>{
+        t.approx = approximatePosition(t, tree);
+    });
 }
 
 
 
 queue()
 	.defer(d3.csv, "map.csv")
+    .defer(d3.json, "testdata.json")
+    //.defer(d3.json, "https://live.at.is/Home/GetTeamListUpdate")
 	.await(buildMap);
+
