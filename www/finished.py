b7aa1f9d23fe4e7beba66b90e33a88ae2008c693
edvaldig <edvaldig@gmail.com>
2017-06-23 08:12:49 +0000

    adapted finished.py to 2017

--- www/finished.py
+++ www/finished.py
@@ -3,20 +3,28 @@ import requests
 import json
 
 namefixes = {
-    "CCP & ZWIFT":"CCP og ZWIFT"
 }
 def fixname(name):
     if name in namefixes:
         return namefixes[name]
     return name
 
-page = requests.get("http://www.timataka.net/wowcyclothon2016/urslit/?race=4")
-tree = html.fromstring(page.content)
-rows = tree.xpath('//table/tbody/tr')
+resultdict = []
+results = [
+    "https://www.timataka.net/wowcyclothon2017/urslit?race=4&cat=overall",
+    "https://www.timataka.net/wowcyclothon2017/urslit?race=8&cat=overall",
+    "https://www.timataka.net/wowcyclothon2017/urslit?race=2&cat=overall",
+    "https://www.timataka.net/wowcyclothon2017/urslit?race=1&cat=overall"
+]
 
-completed = [[col.xpath('.//text()') for col in row.xpath('td')] for row in rows]
-cdict = [{"place":team[0][0], "name":team[1][0], "pace":team[2][0], "time":team[3][0]} for team in completed]
+for url in results:
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    rows = tree.xpath('//table/tbody/tr')
+    completed = [[col.xpath('.//text()') for col in row.xpath('td')] for row in rows]
+    cdict = [{"place":team[0][0], "name":team[1][0], "time":team[2][0]} for team in completed]
+    resultdict.extend(cdict)
 
 with open("finals.json", "w") as f:
-    f.write(json.dumps(cdict, ensure_ascii=False).encode('utf-8'))
+    json.dump(resultdict, f, ensure_ascii=False)
 
